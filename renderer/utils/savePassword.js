4ca611cd0d468cd90700c88b4313758a1768e980
dmacisso <dmacisso@gmail.com>
2022-09-21 15:31:49 -0400

    import to yarn

--- renderer/utils/savePassword.js
+++ renderer/utils/savePassword.js
@@ -1,16 +1,20 @@
 const fs = require('fs');
+// import fs from 'fs';
 const path = require('path');
+// import path from 'path';
 const os = require('os');
+// import os from 'os';
+
 // const chalk = require('chalk');
 
 export const savePassword = (password) => {
   fs.open(path.join(__dirname, '../', 'passwords.txt'), 'a', 666, (e, id) => {
     fs.write(id, password + os.EOL, null, 'utf-8', () => {
       fs.close(id, () => {
-        console.log(('Password saved to passwords.txt'))
-      })
-    })
-  })
-}
+        console.log('Password saved to passwords.txt');
+      });
+    });
+  });
+};
 
 // module.exports = savePassword
